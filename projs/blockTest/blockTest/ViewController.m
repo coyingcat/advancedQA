13ad7583ab5c2c823b69cc2033afcd8381da8337
邓江洲 <707703386@qq.com>
2020-07-06 14:54:26 +0800

    中间相差 %ld G

--- projs/blockTest/blockTest/ViewController.m
+++ projs/blockTest/blockTest/ViewController.m
@@ -42,16 +42,22 @@ int a_global = 10;
     
     static int static_a = 101;
     NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n, a_auto = %d \n", a, a_global, static_a, a_auto);
+    long addr_a = (long)&a;
+    NSLog(@"定义前：16 进制 %p，10 进制 %ld ", &a, (long)&a);         //栈区
     
-
+    
     void (^foo)(void) = ^{
         a = 1;
+        NSLog(@"block内部：16 进制 %p，10 进制 %ld ", &a, (long)&a);    //堆区
         a_global = 20;
         static_a = 200;
         a_auto = 666;
+        NSLog(@"中间相差 %ld M", (addr_a - (long)&a)/(1024*1024));
+        NSLog(@"中间相差 %ld G", (addr_a - (long)&a)/(1024*1024*1024));
     };
     
     foo();
+    NSLog(@"定义后：16 进制 %p，10 进制 %ld ", &a, (long)&a);         //堆区
     
     NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n, a_auto = %d \n", a, a_global, static_a, a_auto);
     
