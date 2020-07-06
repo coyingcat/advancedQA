548bfed141d2f3731831c6680907723ad730ef61
邓江洲 <707703386@qq.com>
2020-07-06 14:18:57 +0800

    __block auto

--- projs/blockTest/blockTest/ViewController.m
+++ projs/blockTest/blockTest/ViewController.m
@@ -32,22 +32,28 @@ int a_global = 10;
     
     __block int a = 0;
     
+    __block auto int a_auto = 13;
+    
     
     //  加 static (放在静态存储区/全局初始化区 )
     //  缺点是, 会永久存储，内存开销大
+    
+    //  原理是 block 内部对外部 auto 变量进行指针捕获
+    
     static int static_a = 101;
-    NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n", a, a_global, static_a);
+    NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n, a_auto = %d \n", a, a_global, static_a, a_auto);
     
 
     void (^foo)(void) = ^{
         a = 1;
         a_global = 20;
         static_a = 200;
+        a_auto = 666;
     };
     
     foo();
     
-    NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n", a, a_global, static_a);
+    NSLog(@"a = %d,\n a_global = %d \n, static_a = %d \n, a_auto = %d \n", a, a_global, static_a, a_auto);
     
     
     
